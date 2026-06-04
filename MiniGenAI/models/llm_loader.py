d33083077a7895599da745c57f1c8e59ef7f1c37
Pushpanjali0410 <pushpanjalisivaprasad@gmail.com>
2026-06-04 17:39:37 +0530

    Mini Generative AI Assistant

--- MiniGenAI/models/llm_loader.py
+++ MiniGenAI/models/llm_loader.py
@@ -1,16 +1,11 @@
-from transformers import pipeline
-from langchain_community.llms import HuggingFacePipeline
-
+from langchain_huggingface import HuggingFacePipeline
 
 def load_llm():
-
     pipe = pipeline(
         "text-generation",
         model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
         max_new_tokens=256,
-        temperature=0.7
+        temperature=0.7,
+        device=-1  # Force CPU usage
     )
-
-    return HuggingFacePipeline(
-        pipeline=pipe
-    )
+    return HuggingFacePipeline(pipeline=pipe)
