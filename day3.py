17a7f7e5d61cd5c5a037323818d3cbe6e042bc67
Pushpanjali0410 <pushpanjalisivaprasad@gmail.com>
2026-06-10 14:03:26 +0530

    Performance Metrics Comparison

--- day3.py
+++ day3.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score,recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
